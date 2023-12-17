e70fe74a100c35479a416e743a6ac90aef48cbe3
Manish Kumar <manish481181@gmail.com>
2023-12-17 13:39:45 +0530

    build for deployement

--- Budget Calculator/src/App.jsx
+++ Budget Calculator/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     <>
         <Container className="my-4">
           <Stack direction="horizontal" gap={2} className="mb-4">
-            <h1 className="me-auto">Budget</h1>
+            <h1 className="me-auto">Budgets</h1>
             <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
             <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
           </Stack>
